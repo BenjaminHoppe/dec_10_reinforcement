148a0fe4974b33a1e3b0d3ddc7a6054cb47d1211
Benjamin <hoppeconnect@gmail.com>
2018-12-13 14:38:48 -0500

    rearranged some stuff

--- ex1.rb
+++ ex1.rb
@@ -1,12 +1,12 @@
 nums = {}
 
 (1..50).each do |item|
-  if item % 2 == 0
+  if item % 2 == 0 && item % 7 == 0
+    nums[item] = item * 2
+  elsif item % 2 == 0
     nums[item] = item + 1
   elsif item % 7 == 0
     nums[item] = item - 1
-  elsif item % 2 == 0 && item % 7 == 0
-    nums[item] = item * 2
   else
     nums[item] = item
   end
